8305cf47faa38a2f617302a2a2ac75bbbd24bcd0
taylortreece <tayloratreece@gmail.com>
2021-11-13 20:05:31 -0600

    commit tree 75ede60979abe7a2687965d482437ae0ae73c97f

--- index.js
+++ index.js
@@ -1,62 +1,12 @@
-function getName(node){
-    return node.name;
+function getName(node) {
+  return node.name
 }
 
-function next(node, collection){
-  let nextAddress = node.next
-  return collection[`${nextAddress}`]
+function headNode(linkedList, collection) {
+  return collection[linkedList]
 }
 
-function addressAt(index, linkedList, collection){
-    if(index == 0) {
-      return linkedList
-    } else {
-      let node = nodeAt(index-1, linkedList, collection)
-      return node.next
-    }
+function next(node, linkedList, collection) {
+  console.log(node)
+  return collection
 }
-
-function headNode(linkedList, collection){
-    return collection[linkedList]
-}
-
-function nodeAt(index, linkedList, collection){
-    let currentNode = headNode(linkedList, collection);
-    for(let i = 0; i < index; i++){
-       currentNode = next(currentNode, collection);
-    }
-  
-    return currentNode;
-  }
-  
-  function indexAt(node, collection, linkedList){
-    let currentNode = headNode(linkedList, collection);
-    let currentIdx = 0
-    while(currentNode != node){
-      currentIdx++
-      currentNode = next(currentNode, collection)
-    }
-    return currentIdx
-  }
-  
-  
-  function insertNodeAt(index, newNodeAddress, linkedList, collection){
-    let previousNode = nodeAt(index - 1, linkedList, collection)
-    let subsequentNode = nodeAt(index, linkedList, collection)
-  
-    let subsequentNodeAddress = addressAt(subsequentNode, linkedList, collection)
-    previousNode.next = newNodeAddress
-    let newNode = collection[newNodeAddress]
-    newNode.next = subsequentNodeAddress
-  }
-  
-  
-  function deleteNodeAt(index, linkedList, collection){
-    let previousNode;
-    let currentNode = headNode(linkedList, collection);
-    for(let i = 0; i < index; i++){
-       previousNode = currentNode
-       currentNode = next(currentNode, collection);
-    }
-    previousNode.next = currentNode.next
-  }
--- test/index-test.js
+++ test/index-test.js
@@ -28,7 +28,7 @@ describe('#next', function() {
     let firstNode = {name: 'susie', next: 'rkjasj'}
     let secondNode = {name: 'sam', next: 'asnan'}
     let lastNode = {name: 'charlie', next: null}
-    let collection = {rkjasj: secondNode, asnan: lastNode, whana: firstNode}
+    let collection = {"rkjasj": secondNode, asnan: lastNode, whana: firstNode}
     let linkedList = 'whana'
     let head = collection[linkedList]
     expect(next(head, collection)).toEqual(secondNode)
